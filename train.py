515649cd585b08d3622628ba325d69a1c8bffa04
Erling Olweus <erlingolweus@gmail.com>
2021-02-25 23:16:37 +0100

    fix error with num_workers

--- train.py
+++ train.py
@@ -16,17 +16,17 @@ from utils import (
 # Hyperparameters etc.
 LEARNING_RATE = 1e-4
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
-BATCH_SIZE = 16
+BATCH_SIZE = 5
 NUM_EPOCHS = 3
-NUM_WORKERS = 10
+NUM_WORKERS = 1
 IMAGE_HEIGHT = 160  # 1280 originally
 IMAGE_WIDTH = 240  # 1918 originally
 PIN_MEMORY = True
 LOAD_MODEL = False
-TRAIN_IMG_DIR = "data/train_images/"
-TRAIN_MASK_DIR = "data/train_masks/"
-VAL_IMG_DIR = "data/val_images/"
-VAL_MASK_DIR = "data/val_masks/"
+TRAIN_IMG_DIR = "/data/train_images/"
+TRAIN_MASK_DIR = "/data/train_masks/"
+VAL_IMG_DIR = "/data/val_images/"
+VAL_MASK_DIR = "/data/val_masks/"
 
 def train_fn(loader, model, optimizer, loss_fn, scaler):
     loop = tqdm(loader)                                     # TODO: take a look at tqdm for progress bars
